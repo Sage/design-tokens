476d50c64b3b816854ff7c96e7eac1cab9d4875c
MarkMizenSagecom <mark.mizen@sage.com>
2021-02-22 14:21:10 +0000

    update to pick util

--- scripts/_utils/pick.js
+++ scripts/_utils/pick.js
@@ -2,11 +2,11 @@
 Copyright 2021
 The Sage Group plc.
  */
-function pick (input, allowedList) {
-  // Source: https://gist.github.com/bisubus/2da8af7e801ffd813fab7ac221aa7afc
-  return Object.keys(input)
-    .filter((key) => allowedList.indexOf(key) >= 0)
-    .reduce((newObj, key) => Object.assign(newObj, { [key]: input[key] }), {})
+function pick (input, allowedList = []) {
+  return Object.assign(
+    {},
+    ...allowedList.map((key) => ({ [key]: input[key] }))
+  )
 }
 
 module.exports = pick
