9cbe72b1826e626eeb30712dd31eac5cfa9b4c62
MarkMizenSagecom <mark.mizen@sage.com>
2021-02-22 14:22:25 +0000

    added docstring back

--- scripts/_utils/pick.js
+++ scripts/_utils/pick.js
@@ -2,6 +2,16 @@
 Copyright 2021
 The Sage Group plc.
  */
+
+/**
+ * Strips all properties from an object except those that are in the allowdList
+ * not recursive
+ *
+ * @param {Object} input - The input object to "pick from"
+ * @param {String[]} allowedList - The list of allowed properties
+ *
+ * @returns {Object}
+ */
 function pick (input, allowedList = []) {
   return Object.assign(
     {},
