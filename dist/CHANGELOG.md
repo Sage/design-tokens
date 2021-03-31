1839ed0103eb17de7ad06a4a52e676abce3e3f25
MarkMizenSagecom <mark.mizen@sage.com>
2021-03-31 10:56:51 +0100

    fix(package): Update to publishing workflow

--- dist/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-## [1.0.1](https://github.com/Sage/design-tokens/compare/v1.0.0...v1.0.1) (2021-03-31)
-
-
-### Bug Fixes
-
-* **package:** made package public ([81c1c3a](https://github.com/Sage/design-tokens/commit/81c1c3a54641a96f12461d8d4310f3a6a6b30648))
--- release.config.js
+++ release.config.js
@@ -1,14 +1,39 @@
 /*
 Copyright © 2021 The Sage Group plc or its licensors. All Rights reserved
  */
+const DIST = 'dist'
+const CHANGELOG = 'docs/CHANGELOG.md'
+
 module.exports = {
   branches: ['master'],
   plugins: [
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/git',
-    '@semantic-release/github'
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: CHANGELOG
+      }
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        changelogFile: CHANGELOG,
+        pkgRoot: DIST,
+        tarballDir: DIST
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [CHANGELOG]
+      }
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: `${DIST}/*.tgz`
+      }
+    ]
   ],
   dryRun: false,
   debug: false
