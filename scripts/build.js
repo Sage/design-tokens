71879ab4611cefeb6c5d52994c0a574301091573
Jakub Hajduk <ext-jakub.hajduk@sage.com>
2021-05-07 15:52:06 +0200

    ci(update tokens): modified build script

--- scripts/build.js
+++ scripts/build.js
@@ -3,152 +3,51 @@ Copyright © 2021 The Sage Group plc or its licensors. All Rights reserved
  */
 const path = require('path')
 const {
-  STRING_SPACES,
-  NAME_CONSTANT,
   registerTransforms
 } = require('./_utils/customTransforms')
 
-const isMediumThemeValue = (token) =>
-  token.type === 'color' && token.path[1] === 'small'
-const isSmallThemeValue = (token) =>
-  token.type === 'color' && token.path[1] === 'medium'
+// const isMediumThemeValue = (token) =>
+//   token.type === 'color' && token.path[1] === 'small'
+// const isSmallThemeValue = (token) =>
+//   token.type === 'color' && token.path[1] === 'medium'
 
 const StyleDictionary = require('style-dictionary').extend({
   source: [path.resolve(__dirname, '../data/**/*.json')],
   platforms: {
-    css: {
-      transformGroup: 'css',
-      buildPath: 'dist/css/',
-      transforms: [
-        'attribute/cti',
-        'name/cti/kebab',
-        'color/hex',
-        'number/units',
-        STRING_SPACES
-      ],
-      files: [
-        {
-          destination: '_variables.css',
-          format: 'css/variables',
-          filter: (token) => token.category === 'theme' || token.category === 'fill',
-          options: {
-            showFileHeader: false
-          }
-        }
-      ]
-    },
     js: {
-      transformGroup: 'js',
       buildPath: 'dist/js/',
       transforms: [
         'attribute/cti',
-        'name/ti/constant',
-        'color/hex',
-        'number/units'
-      ],
-      files: [
-        {
-          destination: 'color.js',
-          format: 'javascript/es6',
-          filter: (token) => token.type === 'color',
-          options: {
-            showFileHeader: false
-          }
-        },
-        {
-          destination: 'spacing.js',
-          format: 'javascript/es6',
-          filter: (token) => token.type === 'spacing',
-          options: {
-            showFileHeader: false
-          }
-        },
-        {
-          destination: 'size.js',
-          format: 'javascript/es6',
-          filter: (token) => token.category === 'size',
-          options: {
-            showFileHeader: false
-          }
-        },
-        {
-          destination: 'text.js',
-          format: 'javascript/es6',
-          filter: (token) => token.attributes.category === 'text',
-          options: {
-            showFileHeader: false
-          }
-        }
-      ]
-    },
-    jsThemes: {
-      transformGroup: 'js',
-      buildPath: 'dist/js/themes/',
-      transforms: [
-        'attribute/cti',
-        NAME_CONSTANT,
-        'color/hex',
-        'number/units'
-      ],
-      files: [
-        {
-          destination: 'small.js',
-          format: 'javascript/es6',
-          filter: isSmallThemeValue,
-          options: {
-            showFileHeader: false
-          }
-        },
-        {
-          destination: 'medium.js',
-          format: 'javascript/es6',
-          filter: isMediumThemeValue,
-          options: {
-            showFileHeader: false
-          }
-        }
-      ]
-    },
-    scss: {
-      transformGroup: 'scss',
-      buildPath: 'dist/scss/',
-      transforms: [
-        'attribute/cti',
-        'name/cti/kebab',
+        'name/cti/constant',
         'color/hex',
         'number/units',
-        STRING_SPACES
+        'custom/name/flattenReference'
       ],
       files: [
         {
-          destination: '_variables.scss',
-          format: 'scss/variables',
+          destination: 'variables.js',
+          format: 'javascript/es6',
+          filter: (token) => {
+            return token.attributes.type === 'accent'
+          },
           options: {
-            showFileHeader: false
-          }
-        }
-      ]
-    },
-    less: {
-      transformGroup: 'less',
-      buildPath: 'dist/less/',
-      transforms: [
-        'attribute/cti',
-        'name/cti/kebab',
-        'color/hex',
-        'number/units',
-        STRING_SPACES
-      ],
-      files: [
-        {
-          destination: '_variables.less',
-          format: 'less/variables',
-          options: {
-            showFileHeader: false
+            showFileHeader: false,
+            outputReference: false
           }
         }
       ]
     }
+  }
+})
+
+StyleDictionary.registerTransform({
+  name: 'custom/name/flattenReference',
+  type: 'value',
+  matcher: function (prop) {
+    return prop.value.toString().startsWith('{')
+  },
+  transformer: function (prop) {
+    return prop
   }
 })
 
