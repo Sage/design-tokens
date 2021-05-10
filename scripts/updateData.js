1e0171f2abcb2fcfc4ad99b2ffc3abc63294d641
MarkMizenSagecom <mark.mizen@sage.com>
2021-05-10 10:37:30 +0100

    ci(update data): Tokens are now saved to a single file

--- scripts/updateData.js
+++ scripts/updateData.js
@@ -17,61 +17,21 @@ async function deleteExistingData () {
   await fs.ensureDir(dataFolder)
 }
 
-// Parses tokens data object to format compatible with style-dictionary.
-function parseOutput (category, tokensData) {
-  const walk = (item) => {
-    switch (typeof item) {
-      case 'string':
-      case 'number':
-        return { value: item }
-      case 'object': {
-        if (item.value) {
-          return item
-        }
-
-        const output = Object
-          .keys(item)
-          .map((key) => [key, walk(item[key])])
-
-        return Object.fromEntries(output)
-      }
-    }
-  }
-
-  return {
-    [category]: walk(tokensData)
-  }
-}
-
 // Splits the tokens into seperate files
-async function writeNewData () {
-  // Split the incoming data into seperate files and directories based on top level category
-  const writeFiles = Object.keys(tokenValues).map((namespace) => {
-    const directory = path.resolve(__dirname, `../data/${namespace}`)
-    fs.ensureDirSync(directory)
-
-    return Object.keys(tokenValues[namespace]).map((category) => {
-      const output = parseOutput(category, tokenValues[namespace][category])
-      const filePath = path.resolve(directory, `${category}.json`)
-
-      console.log('Creating file', filePath)
-
-      return fs.writeJson(
-        filePath,
-        output,
-        {
-          spaces: 4
-        }
-      )
-    })
-  })
-
-  await Promise.all(writeFiles.flat())
+async function writeNewData (data) {
+  const filePath = path.resolve(__dirname, '../data/tokens.json')
+  return fs.writeJson(
+    filePath,
+    data,
+    {
+      spaces: 4
+    }
+  )
 }
 
 async function main () {
   await deleteExistingData()
-  await writeNewData()
+  await writeNewData(tokenValues)
   // Any cleanup actions can be added here
 }
 
