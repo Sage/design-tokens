ab432cb4d905dc6ab0a52f42e46d5952d6e8aad5
Jakub Hajduk <ext-jakub.hajduk@sage.com>
2021-05-10 12:50:21 +0200

    ci(update tokens) added JSDoc for fetchReferenceValue function

--- scripts/_utils/fetchReferenceValue.js
+++ scripts/_utils/fetchReferenceValue.js
@@ -2,6 +2,13 @@
 Copyright © 2021 The Sage Group plc or its licensors. All Rights reserved
  */
 
+/**
+ * Function that fetches value if it is a reference value in form
+ * of path inside brackets, e.g. {set.category.name.variant}
+ *
+ * @param token
+ * @returns {string|number}
+ */
 module.exports = function (token) {
   if (String(token.original?.value).startsWith('{') && typeof token.value === 'object') {
     return token.value?.value
