284c540a5341fbc022806e0857287e0b78e698d4
Jakub Hajduk <jhajduk@pgs-soft.com>
2021-07-23 14:11:12 +0200

    ci(build): Fixed transforms and formats to provide proper formats of tokens

--- scripts/extensions/formats.js
+++ scripts/extensions/formats.js
@@ -1,24 +1,14 @@
 /*
 Copyright © 2021 The Sage Group plc or its licensors. All Rights reserved
  */
-const setWith = require('lodash/setWith')
-const fileHeader = require('../_utils/fileHeader.js')
 
 module.exports = function (styleDictionary) {
   styleDictionary.registerFormat({
-    name: 'custom/js/constant',
-    formatter: function (dictionary) {
-      const outputObject = {}
-
-      dictionary.allProperties.forEach((token) => {
-        setWith(outputObject, token.name, token.value, Object)
-      })
-
-      const output = Object.entries(outputObject).map(([constName, constValues]) => {
-        return `export const ${constName} = ${JSON.stringify(constValues, null, 2)}`
-      })
-
-      return fileHeader + '\r\n\r\n' + output.join('\r\n\r\n')
+    name: 'custom/js/es6-module-flat',
+    formatter: function ({ dictionary, platform, file, options }) {
+      const tokens = Object.fromEntries(dictionary.allTokens.map((token) => [token.name, token.value]))
+      const output = JSON.stringify(tokens, null, 2)
+      return `export default ${output}`
     }
   })
 }
--- scripts/extensions/transforms.js
+++ scripts/extensions/transforms.js
@@ -1,114 +1,51 @@
 /*
 Copyright © 2021 The Sage Group plc or its licensors. All Rights reserved
  */
-
-const fixReferenceValue = require('../_utils/fixReferenceValue')
+const camelCase = require('lodash/camelCase')
 
 module.exports = function (styleDictionary) {
-  /**
-   * Sets namespace, category and variant for generic tokens
-   */
-  styleDictionary.registerTransform({
-    name: 'custom/attributes/generic',
-    type: 'attribute',
-    matcher: (token) => token.path.length === 3,
-    transformer (token) {
-      return {
-        namespace: token.path[0],
-        category: token.path[1],
-        variant: token.path[2]
-      }
-    }
-  })
-
   /**
    * Sets namespace, category, name and variant attributes for token.
    */
   styleDictionary.registerTransform({
     name: 'custom/attributes/colors',
     type: 'attribute',
-    matcher: (token) => token.path.length === 5,
     transformer (token) {
-      return {
-        namespace: token.path[0],
-        category: token.path[1],
-        name: token.path[3],
-        variant: token.path[4]
+      if (token.path.length === 3) {
+        const [theme, category, variant] = token.path
+        return { theme, category, variant }
       }
+
+      if (token.path.length === 4) {
+        const [theme, category, name, variant] = token.path
+        return { theme, category, name, variant }
+      }
+
+      if (token.path.length === 5) {
+        const [theme, category, name, subname, variant] = token.path
+        return { theme, category, name, subname, variant }
+      }
+
+      return { }
     }
   })
 
   /**
-   * Transforms name to NAMESPACE_CATEGORY_<PARTS>_NAME_VARIANT
+   * Transforms name to camelCase categoryNameVariant format
    */
   styleDictionary.registerTransform({
-    name: 'custom/name/generic',
+    name: 'custom/name/camel',
     type: 'name',
     transformer (token) {
-      return Object.values(token.attributes).map(part => part.toUpperCase()).join('_')
+      return camelCase(Object.values(token.attributes).slice(1).join(' '))
     }
   })
 
-  /**
-   * Transforms name to NAMESPACE_CATEGORY.path.to.value
-   */
-  styleDictionary.registerTransform({
-    name: 'custom/name/constant-object',
-    type: 'name',
-    transformer (token) {
-      const [namespace, category, ...parts] = token.path
-      const output = `${namespace.toUpperCase()}_${category.toUpperCase()}.${parts.join('.')}`
-
-      return output
-    }
-  })
-
-  /**
-   * Transforms value with spaces to quoted strings
-   */
-  styleDictionary.registerTransform({
-    name: 'custom/value/string-spaces',
-    type: 'value',
-    matcher: function (prop) {
-      return prop.type === 'string' && /\s/.test(prop.value)
-    },
-    transformer: function (prop) {
-      return `'${prop.original.value}'`
-    }
-  })
-
-  /**
-   * Transforms name to uppercase then replaced non alpha-numeric characters with underscores
-   */
-  styleDictionary.registerTransform({
-    name: 'custom/name/generic',
-    type: 'name',
-    transformer: function (prop) {
-      return prop.name.toUpperCase().replace(/[^A-Z\d_]/g, '_')
-    }
-  })
-
-  styleDictionary.registerTransform({
-    name: 'custom/attributes/fix-references',
-    type: 'attribute',
-    matcher (token) {
-      return String(token.value).startsWith('{') || String(token.value).startsWith('$')
-    },
-    transformer (token) {
-      token.value = fixReferenceValue(token.value, token.attributes)
-    }
-  })
-
-  styleDictionary.registerTransform({
-    name: 'custom/attributes/fix-typography-references',
-    type: 'attribute',
-    matcher (token) {
-      return token.attributes.category === 'typography'
-    },
-    transformer (token) {
-      const output = Object.entries(token.value).map(([property, reference]) => [property, fixReferenceValue(reference, token.attributes)])
-
-      token.value = Object.fromEntries(output)
-    }
+  styleDictionary.registerTransformGroup({
+    name: 'web',
+    transforms: [
+      'custom/attributes/colors',
+      'custom/name/camel'
+    ]
   })
 }
