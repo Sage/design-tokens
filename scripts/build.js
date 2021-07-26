d9be96366b5f514105776e650981dc6c2d70a15e
Jakub Hajduk <ext-jakub.hajduk@sage.com>
2021-07-26 10:08:46 +0200

    ci(build): Fixed transforms and formats to provide proper formats of … (#25)
    * ci(build): Fixed transforms and formats to provide proper formats of tokens
    
    * ci(build): Restored dist/package.json and data/tokens.json files.
    
    Co-authored-by: Jakub Hajduk <jhajduk@pgs-soft.com>

--- scripts/build.js
+++ scripts/build.js
@@ -9,19 +9,110 @@ const path = require('path')
 const StyleDictionary = require('style-dictionary').extend({
   source: [path.resolve(__dirname, '../data/**/*.json')],
   platforms: {
-    js: {
+    javascript: {
       buildPath: 'dist/js/',
-      transforms: [
-        'custom/attributes/generic',
-        'custom/attributes/colors',
-        'custom/attributes/fix-references',
-        'custom/name/constant-object',
-        'custom/attributes/fix-typography-references'
-      ],
+      transformGroup: 'web',
       files: [
         {
-          destination: 'variables.js',
-          format: 'custom/js/constant'
+          filter: (token) => token.attributes.theme === 'base',
+          destination: 'base/common.js',
+          format: 'javascript/module-flat'
+        },
+        {
+          filter: (token) => token.attributes.theme === 'base',
+          destination: 'base/es6.js',
+          format: 'custom/js/es6-module-flat'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 's-biz',
+          destination: 's-biz/common.js',
+          format: 'javascript/module-flat'
+        },
+        {
+          filter: (token) => token.attributes.theme === 's-biz',
+          destination: 's-biz/es6.js',
+          format: 'custom/js/es6-module-flat'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 's-serv',
+          destination: 'm-serv/common.js',
+          format: 'javascript/module-flat'
+        },
+        {
+          filter: (token) => token.attributes.theme === 's-serv',
+          destination: 'm-serv/es6.js',
+          format: 'custom/js/es6-module-flat'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 'references',
+          destination: 'references/common.js',
+          format: 'javascript/module-flat'
+        },
+        {
+          filter: (token) => token.attributes.theme === 'references',
+          destination: 'references/es6.js',
+          format: 'custom/js/es6-module-flat'
+        }
+      ]
+    },
+    css: {
+      buildPath: 'dist/css/',
+      transformGroup: 'web',
+      files: [
+        {
+          filter: (token) => token.attributes.theme === 'base',
+          destination: 'base.css',
+          format: 'css/variables'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 's-biz',
+          destination: 's-biz.css',
+          format: 'css/variables'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 's-serv',
+          destination: 'm-serv.css',
+          format: 'css/variables'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 'references',
+          destination: 'references.css',
+          format: 'css/variables'
+        }
+      ]
+    },
+    scss: {
+      buildPath: 'dist/scss/',
+      transformGroup: 'web',
+      files: [
+        {
+          filter: (token) => token.attributes.theme === 'base',
+          destination: 'base.scss',
+          format: 'scss/variables'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 's-biz',
+          destination: 's-biz.scss',
+          format: 'scss/variables'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 's-serv',
+          destination: 'm-serv.scss',
+          format: 'scss/variables'
+        },
+
+        {
+          filter: (token) => token.attributes.theme === 'references',
+          destination: 'references.scss',
+          format: 'scss/variables'
         }
       ]
     }
