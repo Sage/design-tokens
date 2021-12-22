3e22b5d0665a4f6a3f98c0c1db070861bca676df
Joanna Mikulewicz <ext-joanna.mikulewicz@sage.com>
2021-12-22 09:48:19 +0100

    feat(boxShadow): update boxShadow transformations for css and remove unnecessry custom-transform files (#142)
    * feat(boxShadow): update boxShadow transformations for css and remove unnecessry custom-transform files
    
    * refactor(boxShadow): rename array elements, return string instead of array

--- scripts/transforms/custom-value-css-box-shadow.transform.js
+++ scripts/transforms/custom-value-css-box-shadow.transform.js
@@ -6,6 +6,8 @@ Copyright © 2021 The Sage Group plc or its licensors. All Rights reserved
  * Handles boxShadow tokens
  */
 
+const isArray = require('lodash/isArray')
+
 module.exports = {
   name: 'custom/value/css-box-shadow',
   type: 'value',
@@ -15,6 +17,17 @@ module.exports = {
 
     if (value.color === 'none') {
       return 'none'
+    }
+
+    if (isArray(value)) {
+      return value.map(shadowDefinition => {
+        const boxShadow = `${shadowDefinition.x}px ${shadowDefinition.y}px ${shadowDefinition.blur}px ${shadowDefinition.spread}px ${shadowDefinition.color}`
+        if (shadowDefinition.type === 'innerShadow') {
+          return `inset ${boxShadow}`
+        }
+
+        return boxShadow
+      }).join(', ')
     }
 
     return `${value.x} ${value.y} ${value.blur} ${value.spread} ${value.color}`
