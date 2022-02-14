e9412e30fb722dd4e66cd26feb389d7672f0e080
Jakub Hajduk <ext-jakub.hajduk@sage.com>
2022-02-14 15:45:56 +0100

    feat: bump version number in main package.json

---
+++ scripts/bump-main-package-version.js
@@ -0,0 +1,22 @@
+/*
+Copyright © 2021 The Sage Group plc or its licensors. All Rights reserved
+ */
+const { resolve } = require('path')
+const { readJsonSync, outputJsonSync } = require('fs-extra')
+
+console.log('Bumping version in main package.json file...')
+try {
+  const mainPackageJsonFilePath = resolve(__dirname, '../package.json')
+  const distPackageJsonFilePath = resolve(__dirname, '../dist/package.json')
+
+  const version = readJsonSync(distPackageJsonFilePath).version
+  const mainPackageJson = readJsonSync(mainPackageJsonFilePath)
+  const outputPackageJson = { ...mainPackageJson, version }
+
+  outputJsonSync('./package.json', outputPackageJson, { spaces: 2 })
+
+  console.log('Done.\r\n')
+} catch (err) {
+  console.log('Error!\r\n')
+  console.log(err)
+}
