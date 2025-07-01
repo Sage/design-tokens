a3fe4fbb3dcdbd5f0bb30332654814d3b0a5b33d
Luke Emmerson-Finch <luke.emmersonfinch@sage.com>
2025-07-01 11:18:58 +0100

    feat!: update token structure to remove product contexts and screen size tokens
    - Removes `frozenproduct` and `product` contexts from output.
    - Removes screen size level to token output.
    - Removes `all.css` file from output.
    - Removes `ios` format from output.
    - Renames `typography` tokens to `font`.
    - Removes `form` and `status` tokens.
    - Removes specific `spacing`, `font`, `size`, `radius`, `borderWidth`
    and `shadow` tokens for `components` in favour of `global`.
    - Removes comments from output.
    - Ensures `components` tokens in all outputted formats resolve to
    references rather than values to support white-labelling.
    - Adds `message` and `pill` to `components` tokens.
    - Adds `inverse` tokens for the following `components`:
      - `container` (Tile and Card) status borders.
      - `input` validation bars and label sets.
      - Sage `logo`.
      - AI `sparkle` and `loader`.

--- scripts/post-process-css/formatters/index.ts
+++
@@ -1,4 +0,0 @@
-export { ConsolidateScreenSizes } from "./consolidate-screen-sizes/consolidate-screen-sizes";
-export { FilterTypographyTokens } from "./filter-typography-tokens/filter-typography-tokens";
-export { LightDarkModeFormatter } from "./light-dark-mode/light-dark-mode-formatter";
-export { MathsCalc } from "./maths-calc/maths-calc";
